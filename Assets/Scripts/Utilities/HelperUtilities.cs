f5fb987452215a64ecf928cab9bb6f3210dae9c3
Dendy Darin <dendy050677@gmail.com>
2023-12-02 00:13:31 +0900

    Add Scriptable Objects for NodeGraph

---
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public static class HelperUtilities
+{
+    // empty string debug check
+    public static bool ValidateCheckEmptyString(Object thisObject, string fieldName, string stringToCheck)
+    {
+        if (stringToCheck == "")
+        {
+            Debug.Log(fieldName + " is empty and must contain a value in object " + thisObject.name.ToString());
+            return true;
+        }
+        return false;
+    }
+
+    // list empty or contains null value check - returns true if there is an error
+    public static bool ValidateCheckEnumerableValue(Object thisObject, string fieldName, IEnumerable enumerableObjeckToCheck)
+    {
+        bool error = false;
+        int count = 0;
+
+        foreach (var item in enumerableObjeckToCheck)
+        {
+            if (item == null)
+            {
+                Debug.Log(fieldName + " has null value in object " + thisObject.name.ToString());
+                error = true;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            Debug.Log(fieldName + " has no value in object " + thisObject.name.ToString());
+            error = true;
+        }
+
+        return error;
+    }
+}
